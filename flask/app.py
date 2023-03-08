ad8d99c9aa0f02974836b9addc4ffe19b69e37e0
Benjamin Knower <ben.knower@gmail.com>
2023-03-08 17:51:05 -0500

    basic front end functionality

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from scraper import get_repositories
 app = Flask(__name__)
+CORS(app)
 
 cache = {}
 @app.route('/user/<username>')
@@ -11,3 +13,7 @@ def profile(username):
         repos = get_repositories(username)
         cache[username] = repos
         return repos
+
+@app.route("/users")
+def users():
+    return [user for user in cache]
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,12 +6,44 @@ import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 import React, { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState("");
+  const [userData, setUserData] = useState([]);
+  useEffect(() => {
+    console.log("get users");
+    fetch("http://localhost:5000/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+  useEffect(() => {
+    fetch(`http://localhost:5000/user/${selectedUser}`)
+      .then((response) => response.json())
+      .then((data) => setUserData(data));
+  }, [selectedUser]);
   return (
     <div>
-      <Button variant="contained">Hello World</Button>
+      <Select
+        value={selectedUser}
+        label="User"
+        onChange={(e) => {
+          setSelectedUser(e.target.value);
+        }}
+      >
+        {users.map((user) => (
+          <MenuItem key={user} value={user}>
+            {user}
+          </MenuItem>
+        ))}
+      </Select>
+      <br />
+      {userData.map((repo) => (
+        <div>{repo}</div>
+      ))}
     </div>
   );
 }
